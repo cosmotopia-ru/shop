e8bb47d751da6c8f121d87ba9b66490ac0daac91
cosmotopia-ru <cosmotopia.ru@gmail.com>
2025-10-31 14:19:26 +0300

    Improve cart modal focus management

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-export default function Header({ cartCount }) {
+export default function Header({ cartCount, onCartClick, isCartOpen }) {
   return (<>
     <header>
       <div className='container header-inner'>
         <span className='logo'>House Shop</span>
         <div className='header-actions'>
-          <span className='cart-badge' title='Cart items'>{cartCount}</span>
+          <button
+            type='button'
+            className='cart-button'
+            onClick={onCartClick}
+            aria-haspopup='dialog'
+            aria-expanded={isCartOpen}
+            aria-label={`Open cart. ${cartCount} item${cartCount === 1 ? '' : 's'} in cart.`}
+          >
+            <span className='cart-icon' aria-hidden='true'>🛒</span>
+            <span className='cart-label'>Cart</span>
+            <span className='cart-badge' title='Cart items'>{cartCount}</span>
+          </button>
         </div>
       </div>
       <div className='presentation'></div>
